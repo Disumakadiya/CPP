76da17a9c6f7dcdc7fd0ee6d275c6ac62b41393c
Disumakadiya <disumakadiya@gmail.com>
2025-07-30 01:32:02 +0530

    add

---
+++ Program/codechef_problems/sale_season.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int X;
+        cin>>X;
+        
+        if(X<=100)
+        {
+            cout<<X<<endl;
+        }
+        else if(100<X && X<=1000)
+        {
+            cout<<X-25<<endl;
+        }
+        else if(1000<X&& X<=5000)
+        {
+            cout<<X-100<<endl;
+        }
+        else
+        {
+            cout<<X-500<<endl;
+        }
+    }
+}
