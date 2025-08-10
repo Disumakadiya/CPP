cd4d3f2d4a25c9e09c59e93c3df6a8ad58aaa685
Disumakadiya <disumakadiya@gmail.com>
2025-08-10 10:23:19 +0530

    add

---
+++ Program/codechef_problems/Sugarcane Juice Business.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    int n;
+	    cin>>n;
+	    int profit=(n*50*30)/100;
+	    cout<<profit<<endl;
+   
+	}
+}
