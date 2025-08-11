364ce7cdd99f2098f96c5a0a7bca42ebd61f96dd
Disumakadiya <disumakadiya@gmail.com>
2025-08-12 00:05:48 +0530

    add

---
+++ Program/codechef_problems/runningcomparisons.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n],b[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        for(int i=0;i<n;i++){
+            cin>>b[i];
+        }
+        int count=0;
+        for(int i =0;i<n;i++)
+        if(2*a[i]>=b[i] && a[i]<=2*b[i])
+        {
+            count++;
+        }
+        cout<<count<<endl;
+        
+    }
+}
