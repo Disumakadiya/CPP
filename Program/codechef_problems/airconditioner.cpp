a1363a8675ab9b68f16ecb94565930f15eee42c3
Disumakadiya <disumakadiya@gmail.com>
2025-08-24 10:16:18 +0530

    add

---
+++ Program/codechef_problems/airconditioner.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	     int L = max(a, c);
+        cout << (L <= b ? "Yes" : "No") << "\n";
+	}
+}
