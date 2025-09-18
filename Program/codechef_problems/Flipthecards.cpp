b4d10801e0662968f430fd44aa94860fb5560a1f
Disumakadiya <disumakadiya@gmail.com>
2025-09-18 22:55:41 +0530

    Add

---
+++ Program/codechef_problems/Flipthecards.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int n,x;
+        cin>>n>>x;
+        int ans=min(x,n-x);
+        cout<<ans<<endl;
+    }
+}
