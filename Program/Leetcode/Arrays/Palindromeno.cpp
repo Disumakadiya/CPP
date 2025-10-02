9fee44f8ada5cb10dbfd358e3cd20fa6c4416238
Disumakadiya <disumakadiya@gmail.com>
2025-10-03 00:17:56 +0530

    Add

---
+++ Program/Leetcode/Arrays/Palindromeno.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // string s=to_string(x);
+        // string rev=s;
+        // reverse(rev.begin(),rev.end());
+        // return s==rev;
+
+        if(x<0 || x%10==0 && x!=0)
+        return false;
+
+        int rev=0;
+        while(x>rev)
+        {
+            rev=rev*10+x%10;
+            x/=10;
+        }
+
+        return (x==rev ||x== rev /10);
+    
+
+    }
+};
