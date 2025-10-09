a5ffb790e078ae376a4b14a62ebf80933a730e8e
Disumakadiya <disumakadiya@gmail.com>
2025-10-09 22:40:02 +0530

    add

---
+++ Program/codechef_problems/CandyDistribution.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T; 
+
+    while (T--) {
+        int N, M;
+        cin >> N >> M;
+
+        if (N % M == 0) {
+            int candiesPerFriend = N / M;
+            if (candiesPerFriend % 2 == 0)
+                cout << "Yes\n";
+            else
+                cout << "No\n";
+        } else {
+            cout << "No\n";
+        }
+    }
+
+    return 0;
+}
