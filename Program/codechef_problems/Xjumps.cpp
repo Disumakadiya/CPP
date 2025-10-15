b594b83498cffa45326e35d62403ec072b233ebe
Disumakadiya <disumakadiya@gmail.com>
2025-10-15 23:34:38 +0530

    add

---
+++ Program/codechef_problems/Xjumps.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int x, y, step;
+        cin >> x >> y;
+
+        if (y > x) {
+            step = x;
+        else if (x % y == 0) {
+            step = x / y; 
+        } 
+        else {
+            int full_jumps = x / y;    // number of Y jumps
+            int remaining = x % y;     // leftover stairs
+            step = full_jumps + remaining; // add 1-step for each leftover
+        }
+
+        cout << step << endl;
+    }
+
+    return 0;
+}
